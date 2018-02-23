65937be6e307f76d7c97b33c00f85d10cc785806
Frank Ngo <frankngomusic@gmail.com>
2018-02-23 12:02:35 -0800

    getall() successful pass

--- HairSalon.Tests/ModelsTests/StylistTest.cs
+++ HairSalon.Tests/ModelsTests/StylistTest.cs
@@ -9,55 +9,22 @@ namespace HairSalon.Models.Tests
   public class StylistTest : IDisposable
   {
     public void Dispose()
-     {
-       // Client.DeleteAll();
-       // Stylist.DeleteAll();
-     }
+    {
+      // Client.DeleteAll();
+      // Stylist.DeleteAll();
+    }
+    public StylistTest()
+    {
+      DBConfiguration.ConnectionString = "server=localhost;user id=root;password=root;port=8889;database=frank_ngo_test;";
+    }
+    [TestMethod]
+    public void GetAll_CategoriesEmptyAtFirst_0()
+    {
+      //arrange, act
+      int result = Stylist.GetAll().Count;
 
-     public StylistTest()
-     {
-       DBConfiguration.ConnectionString = "server=localhost;user id=root;password=root;port=8889;database=frank_ngo_test;";
-     }
-
-     [TestMethod]
-     public void GetAll_ListEmpty_0()
-     {
-       //arrange, act
-       int result = Stylist.GetAll().Count;
-
-       //Assert
-       Assert.AreEqual(0, result);
-     }
-
-     [TestMethod]
-     public void Add_AddStylistToList_StylistsList()
-     {
-       //arrange
-       Stylist testStylist = new Stylist("Frank Ngo");
-       testStylist.Add();
-
-       //act
-       List<Stylist> result = Stylist.GetAll();
-       List<Stylist> testList = new List<Stylist>{testStylist};
-
-       //assert
-       CollectionAssert.AreEqual(testList, result);
-     }
-
-     [TestMethod]
-     public void Add_AddStylistToList_StylistsList()
-     {
-       //arrange
-       Stylist testStylist = new Stylist("Frank Ngo");
-       testStylist.Add();
-
-       //act
-       List<Stylist> result = Stylist.GetAll();
-       List<Stylist> testList = new List<Stylist>{testStylist};
-
-       //assert
-       CollectionAssert.AreEqual(testList, result);
-     }
-
+      //Assert
+      Assert.AreEqual(0, result);
+    }
   }
 }
