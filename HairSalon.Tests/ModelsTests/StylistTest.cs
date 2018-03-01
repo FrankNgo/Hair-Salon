1ec65e1d3bf6aefef7408062be3d058a970474db
Frank Ngo <frankngomusic@gmail.com>
2018-03-01 13:36:48 -0800

    add test for user input

--- HairSalon.Tests/ModelsTests/StylistTest.cs
+++ HairSalon.Tests/ModelsTests/StylistTest.cs
@@ -17,74 +17,25 @@ namespace HairSalon.Models.Tests
     {
       DBConfiguration.ConnectionString = "server=localhost;user id=root;password=root;port=8889;database=frank_ngo_test;";
     }
-    [TestMethod]
-    public void GetAll_StylistList_empty()
-    {
-      //arrange, act
-      int result = Stylist.GetAll().Count;
-
-      //Assert
-      Assert.AreEqual(0, result);
-    }
 
     [TestMethod]
-    public void Equals_ReturnsTrue_Stylist()
-    {
-      //Arrange, Act
-      Stylist firstStylist = new Stylist("Frank Ngo");
-      Stylist secondStylist = new Stylist("Frank Ngo");
-
-      //assert
-      Assert.AreEqual(firstStylist, secondStylist);
-    }
-
-    [TestMethod]
-    public void Add_SavesStylistToList_StylistList()
+    public void GetStylistsInfo_FetchAllInfo_String()
     {
       //arrange
-      Stylist testStylist = new Stylist("Frank Ngo");
-      testStylist.Add();
+      Stylist newStylist = new Stylist("Frank","Ngo");
+      string testFirstName = "Frank";
+      string testLastName = "Ngo";
+      int testId = 0;
 
       //act
-      List<Stylist> result = Stylist.GetAll();
-      List<Stylist> testList = new List<Stylist>{testStylist};
+      string firstNameResult = newStylist.GetFirstName();
+      string lastNameResult = newStylist.GetLastName();
+      int idResult = newStylist.GetId();
 
       //assert
-      CollectionAssert.AreEqual(testList, result);
-    }
-
-    [TestMethod]
-    public void Add_SavesIdToStylist_StylistId()
-    {
-      //arrange
-      Stylist testStylist = new Stylist("Frank Ngo");
-      testStylist.Add();
-
-      //act
-      Stylist savedStylist = Stylist.GetAll()[0];
-
-      int result = savedStylist.GetId();
-      int testId = testStylist.GetId();
-
-      //assert
-      Assert.AreEqual(testId, result);
-    }
-
-    [TestMethod]
-    public void GetClients_RetrievesAllClientsWithStylistId_ClientList()
-    {
-      Stylist testStylist = new Stylist("Frank Ngo", 1);
-      testStylist.Add();
-
-      Client firstClient = new Client("Frank Ngo", 1, testStylist.GetId());
-      firstClient.Add();
-      Client secondClient = new Client("Frank Ngo", 2,  testStylist.GetId());
-      secondClient.Add();
-
-      List<Client> testClientList = new List<Client> {firstClient, secondClient};
-      List<Client> resultClientList = testStylist.GetClients();
-
-      CollectionAssert.AreEqual(testClientList, resultClientList);
+      Assert.AreEqual(firstNameResult,testFirstName);
+      Assert.AreEqual(lastNameResult,testLastName);
+      Assert.AreEqual(idResult,testId);
     }
   }
 }
